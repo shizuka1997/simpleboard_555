72213f529bd3878718e0c3189be8f5cf71179fd1
shizuka1997 <system.study.1997@gmail.com>
2022-05-15 11:32:21 +0900

    using layouts & add header footer

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,23 @@
-<h1>Posts</h1>
+@extends('layouts.layouts')
+@section('title', 'Simple Board')
+@section('content')
 
-@foreach($posts as $post)
-    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-    <a href="/posts/{{ $post->id }}/edit">Edit</a>
+    @if (session('message'))
+        {{ session('message') }}
+    @endif
 
-    <form action="/posts/{{ $post->id }}" method="POST" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else {return false };">
-        <input type="hidden" name="_method" value="DELETE">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <button type="submit">Delete</button>
-    </form>
-@endforeach
+    <h1>Posts</h1>
 
-<a href="/posts/create">New Post</a>
+    @foreach($posts as $post)
+        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+        <a href="/posts/{{ $post->id }}/edit">Edit</a>
+
+        <form action="/posts/{{ $post->id }}" method="POST" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else {return false };">
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <button type="submit">Delete</button>
+        </form>
+    @endforeach
+
+    <a href="/posts/create">New Post</a> 
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,11 @@
-@if (session('message'))
+@extends('layouts.layouts')
+@section('title', 'Simple Board')
+
+@section('content')
+    @if (session('message'))
         {{ session('message') }}
     @endif
 
     {{ $post->title }}
     {{ $post->content }}
-
-    <a href="/posts/{{ $post->id }}/edit">Edit</a>
-    
+@endsection 
